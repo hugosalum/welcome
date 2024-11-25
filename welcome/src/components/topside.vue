c2c6da1b2ac061e46e3c5a170a58c7eb0bd64996
Hugo Salum <113598745+hugosalum@users.noreply.github.com>
2024-11-25 22:20:19 +0200

    vue upgrade

---
+++ welcome/src/components/topside.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>bobr</h1>
+    <h2>bobr</h2>
+</template>
+<style>
+.h1 {
+    font: comic-sans;
+}
+</style>
---
+++ welcome/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import topside from "../components/topside.vue";
+</script>
+
+<template>
+    <main>
+        <div>
+            <h1>WELCOME!</h1>
+            <topside></topside>
+        </div>
+    </main>
+</template>
